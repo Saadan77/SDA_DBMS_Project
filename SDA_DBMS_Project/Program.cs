d7fb1ead7e174e8f471fcb70856c2e30068d0ab7
Saadan77 <107300960+Saadan77@users.noreply.github.com>
2023-06-20 14:57:25 +0500

    Saadan

--- SDA_DBMS_Project/Program.cs
+++ SDA_DBMS_Project/Program.cs
@@ -11,7 +11,7 @@ namespace SDA_DBMS_Project
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new mainpanel());
+            Application.Run(new Lovely());
         }
     }
 }
