0b90d57d1b7d086d204745b2d4f324c49d343aa2
HayaSaeed992 <hs.dawray992@gmail.com>
2023-06-22 01:53:30 +0500

    Form Attendance View Finalize ----Haya Saeed

--- SDA_DBMS_Project/Forms/FormAttendance.cs
+++ SDA_DBMS_Project/Forms/FormAttendance.cs
@@ -17,7 +17,7 @@ namespace SDA_DBMS_Project.Forms
         {
             public static SqlConnection GetConnection()
             {
-                string connectionString = "Data Source=SAADAN2001\\SAADANSQL;Initial Catalog=Employees;Persist Security Info=True;User ID=Saadan;Password=Saadanbinjawad$500";
+                string connectionString = "Data Source=DESKTOP-TV1A0O3\\SQLEXPRESS;Initial Catalog=Employees;Integrated Security=True";
                 SqlConnection connection = new SqlConnection(connectionString);
                 connection.Open();
                 return connection;
@@ -27,6 +27,8 @@ namespace SDA_DBMS_Project.Forms
         {
             InitializeComponent();
             LoadTheme();
+            pnlOverview.Visible = false;
+            pnlBreakView.Visible = false;
         }
         private void LoadTheme()
         {
@@ -52,6 +54,83 @@ namespace SDA_DBMS_Project.Forms
         private void FormAttendance_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnClockInOutView_Click(object sender, EventArgs e)
+        {
+            pnlClockInOut.Visible = true;
+            pnlOverview.Visible = false;
+            pnlBreakView.Visible = false;
+
+            try
+            {
+                using (SqlConnection connection = Connector.GetConnection())
+                {
+                    SqlCommand command = new SqlCommand("SELECT * FROM ClockInOutView", connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    gridClockView.DataSource = dataTable;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while displaying Clock In Out View: " + ex.Message);
+            }
+        }
+
+        private void btnBreakDurationView_Click(object sender, EventArgs e)
+        {
+            
+            pnlBreakView.Visible = true;
+            pnlClockInOut.Visible = false;
+            pnlOverview.Visible = false;
+
+            try
+            {
+                using (SqlConnection connection = Connector.GetConnection())
+                {
+                    SqlCommand command = new SqlCommand("SELECT * FROM BreakDurationView", connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    gridBreakView.DataSource = dataTable;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while displaying Break View: " + ex.Message);
+            }
+        }
+
+        private void btnOvertimeHoursView_Click(object sender, EventArgs e)
+        {
+            
+            pnlOverview.Visible = true;
+            pnlBreakView.Visible = false;
+            pnlClockInOut.Visible = false;
+
+            try
+            {
+                using (SqlConnection connection = Connector.GetConnection())
+                {
+                    SqlCommand command = new SqlCommand("SELECT * FROM OvertimeCalculationView", connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    gridOvertime.DataSource = dataTable;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while displaying Overtime Calculation View: " + ex.Message);
+            }
         }
     }
 }
