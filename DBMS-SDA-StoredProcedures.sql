7dab2f5c2b2bab2ea421ba8bd3ddc6c1beaca725
Saadan77 <107300960+Saadan77@users.noreply.github.com>
2023-06-23 14:47:27 +0500

    SDA DBMS Project - Completed

--- DBMS-SDA-StoredProcedures.sql
+++ DBMS-SDA-StoredProcedures.sql
@@ -60,3 +60,58 @@ BEGIN
         WHERE EmployeeID = @EmployeeID;
     END
 END;
+
+CREATE PROCEDURE sp_UpdateBenefits
+    @BenefitID INT,
+    @EmployeeID INT,
+    @BenefitType VARCHAR(50),
+    @StartDate DATE,
+    @EndDate DATE,
+	@Descripition varchar(50),
+	@Amount INT
+AS
+BEGIN
+    IF (@EmployeeID IS NOT NULL)
+    BEGIN
+        UPDATE BenefitsManagement
+        SET EmployeeID = @EmployeeID
+        WHERE BenefitID = @BenefitID;
+    END
+
+    IF (@BenefitType IS NOT NULL)
+    BEGIN
+        UPDATE BenefitsManagement
+        SET BenefitType = @BenefitType 
+        WHERE BenefitID = @BenefitID ;
+    END
+
+    -- Add more IF conditions for other parameters and corresponding columns
+
+    IF (@StartDate IS NOT NULL)
+    BEGIN
+        UPDATE BenefitsManagement
+        SET StartDate = @StartDate
+        WHERE BenefitID = @BenefitID ;
+    END
+
+    IF (@EndDate IS NOT NULL)
+    BEGIN
+        UPDATE BenefitsManagement
+        SET EndDate = @EndDate
+        WHERE BenefitID = @BenefitID;
+    END
+
+    IF (@Amount IS NOT NULL)
+    BEGIN
+        UPDATE BenefitsManagement
+        SET Amount = @Amount
+        WHERE BenefitID = @BenefitID;
+    END
+
+    IF (@Descripition IS NOT NULL)
+    BEGIN
+        UPDATE BenefitsManagement
+        SET Description = @Descripition
+        WHERE BenefitID = @BenefitID;
+    END
+END;
